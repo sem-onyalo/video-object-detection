3c050e22751bdbfae823216e0e62b0f948693dd1
Sem Onyalo <sem.onyalo@slalom.com>
2018-04-13 16:31:48 -0400

    added labels to detection boxes

--- main.py
+++ main.py
@@ -16,10 +16,27 @@ from numpy import pi, sin, cos
 
 import cv2 as cv
 
-# built-in modules
-from time import clock
-
 cvNet = None
+
+classNames = { 
+    0: 'background', 1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane',
+    6: 'bus', 7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
+    13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat',
+    18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear',
+    24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag',
+    32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard',
+    37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove',
+    41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle',
+    46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon',
+    51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange',
+    56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut',
+    61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed',
+    67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse',
+    75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven',
+    80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
+    86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush' 
+}
+
 
 class VideoSynthBase(object):
     def __init__(self, size=None, noise=0.0, bg = None, **params):
@@ -85,16 +102,25 @@ def run_detection(img):
     rows = img.shape[0]
     cols = img.shape[1]
     cvNet.setInput(cv.dnn.blobFromImage(img, 1.0/127.5, (300, 300), (127.5, 127.5, 127.5), swapRB=True, crop=False))
-    cvOut = cvNet.forward()
+    detections = cvNet.forward()
 
-    for detection in cvOut[0,0,:,:]:
+    for detection in detections[0,0,:,:]:
         score = float(detection[2])
         if score > 0.3:
-            left = detection[3] * cols
-            top = detection[4] * rows
-            right = detection[5] * cols
-            bottom = detection[6] * rows
-            cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
+            left = int(detection[3] * cols)
+            top = int(detection[4] * rows)
+            right = int(detection[5] * cols)
+            bottom = int(detection[6] * rows)
+            cv.rectangle(img, (left, top), (right, bottom), (23, 230, 210), thickness=2)
+
+            class_id = int(detection[1])
+            if class_id in classNames:
+                label = classNames[class_id] + ": " + str(score)
+                labelSize, baseLine = cv.getTextSize(label, cv.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+                top = max(top, labelSize[1])
+                cv.rectangle(img, (left, top - labelSize[1]), (left + labelSize[0], top + baseLine),
+                    (255, 255, 255), cv.FILLED)
+                cv.putText(img, label, (left, top), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
     return img
 
 if __name__ == '__main__':
